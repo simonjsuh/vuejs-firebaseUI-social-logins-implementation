7fd03a8fa5442d558e66ca0c8216f453050cc633
simonjsuh <simon.j.suh@gmail.com>
2022-03-30 02:46:25 -0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,87 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2 v-if="user">Signed In User: {{ user }}</h2>
+    <div id="firebaseui-auth-container"></div>
+    <div id="loader">Loading...</div>
+    <br>
+    <div v-if="isSignedIn">
+      <button @click="handleSignOut">Sign Out</button>
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import firebaseConfig from '../firebaseConfig';
+
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+
+firebase.initializeApp(firebaseConfig);import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+import { getAuth, signOut } from "firebase/auth";
+
+const auth = getAuth();
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  setup() {
+    const user = ref(null);
+    const isSignedIn = ref(false);
+
+    const uiConfig = {
+      signInFlow: 'popup',
+      signinSuccessUrl: 'http://localhost:8080/',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID
+      ],
+      callbacks: {
+        signInSuccessWithAuthResult: function (authResult) {
+          user.value = authResult.user.displayName;
+          console.log(authResult)
+          isSignedIn.value = true;
+          console.log('Signed in by user ' + user.value);
+ 
+          // so it doesn't refresh the page
+          return false;
+        },
+        uiShown: function() {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none';
+        }
+      }
+    }
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+    const handleSignOut = () => {
+      signOut(auth).then(() => {
+       // Sign-out successful.
+       user.value = null;
+       isSignedIn.value = false;
+       console.log('Signed out');
+       ui.start('#firebaseui-auth-container', uiConfig);
+      }).catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+    }
+
+    return {
+      user,
+      isSignedIn,
+      handleSignOut,
+    }
   }
 }
 </script>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCnCg8miAUi6IHqvK68wysyS37AUGX1my0",
+  authDomain: "practice8-vue3-firebaseui-app.firebaseapp.com",
+  projectId: "practice8-vue3-firebaseui-app",
+  storageBucket: "practice8-vue3-firebaseui-app.appspot.com",
+  messagingSenderId: "623503090265",
+  appId: "1:623503090265:web:92183f62bc14489ce50748",
+  measurementId: "G-6D4L7KKB3W"
+};
